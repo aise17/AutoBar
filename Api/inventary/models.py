e6cef311cb962d0243072303aa1db01792b0cec3
sergio garcia <sergio.martinez-g@hotmail.com>
2020-10-01 11:46:11 +0200

    columna precio combertido a doble con 2 digitos decimales

--- Api/inventary/models.py
+++ Api/inventary/models.py
@@ -10,13 +10,17 @@ class Category(models.Model):
     creation_date = models.DateTimeField(null=True, blank=True)
     modification_date = models.DateTimeField(null=True, blank=True)
     unpublish_date= models.DateTimeField(null=True, blank=True)
+    
+    
+    def __str__(self):
+        return "Categoria - %s" % self.name 
 
 class Product(models.Model):
     name = models.CharField(max_length=50, null=True, blank=True)
     image = models.TextField(null=True, blank=True)
     description = models.TextField(null=True, blank=True,)
     allergy = models.TextField(null=True, blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=5, blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     category = models.ForeignKey(Category, models.CASCADE, null=True)
     
     publish = models.BooleanField(null=True, blank=True)
@@ -28,6 +32,6 @@ class Product(models.Model):
 
 
     def __str__(self):
-        return "Author - %s" % self.name 
+        return "Producto - %s" % self.name 
 
 
