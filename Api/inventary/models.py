764b19a5a1810b7983f68ff377d6debed4558797
sergio garcia <sergio.martinez-g@hotmail.com>
2020-10-17 01:56:21 +0200

    mesas y sitios de preparacion añadidos

--- Api/inventary/models.py
+++ Api/inventary/models.py
@@ -4,19 +4,25 @@ from datetime import timezone
 from django.conf import settings
 
 
+class PreparationSite(models.Model):
+    name = models.CharField(max_length=50, null=True, blank=True)
+
+
+class Mesa(models.Model):
+    name = models.CharField(max_length=50, null=True, blank=True)
+
+
 class Category(models.Model):
     name = models.TextField(null=True, blank=True)
-
     publish = models.BooleanField(null=True, blank=True)
     publish_date = models.DateTimeField(null=True, blank=True)
     creation_date = models.DateTimeField(null=True, blank=True)
     modification_date = models.DateTimeField(null=True, blank=True)
     unpublish_date= models.DateTimeField(null=True, blank=True)
-    
-
-    
+      
     def __str__(self):
         return "Categoria - %s" % self.name 
+
 
 class Product(models.Model):
     name = models.CharField(max_length=50, null=True, blank=True)
@@ -25,17 +31,17 @@ class Product(models.Model):
     allergy = models.TextField(null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     category = models.ForeignKey(Category, models.CASCADE, null=True, related_name='products')
-    
+    category = models.ForeignKey(Category, models.CASCADE, null=True, related_name='products')
+    preparation_site = models.ForeignKey(PreparationSite, models.CASCADE, null=True, related_name='preparation_site')
     publish = models.BooleanField(null=True, blank=True)
     publish_date = models.DateTimeField(null=True, blank=True)
     creation_date = models.DateTimeField(null=True, blank=True)
     modification_date = models.DateTimeField(null=True, blank=True)
     unpublish_date= models.DateTimeField(null=True, blank=True)
 
- 
-
     def __str__(self):
         return "Producto - %s" % self.name 
+
 
 class Orders(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, models.CASCADE, null=True, related_name='user')
@@ -49,9 +55,11 @@ class OrdersProducts(models.Model):
     product = models.ForeignKey(Product, models.CASCADE, null=True, related_name='product',)
     order_product = models.ForeignKey(Orders, models.CASCADE, null=True, related_name='order_product')
     creation_date = models.DateTimeField(null=True, blank=True)
-
+    mesa = models.ForeignKey(Mesa, models.CASCADE, null=True, related_name='mesa')
  
-
     def __str__(self):
         name = self.product
         return "OrdersProducts - {0} - {1}".format( self.id, name )
+
+
+
