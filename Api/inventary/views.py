6aff417d662bba17fbf2fa854539dbab0541a0fe
sergio garcia <sergio.martinez-g@hotmail.com>
2020-10-25 04:08:38 +0100

    Update views.py

--- Api/inventary/views.py
+++ Api/inventary/views.py
@@ -142,7 +142,7 @@ class CreateListAddress(generics.ListCreateAPIView):
 
         except Exception as ex:
             salida['ok'] = False
-            salida['error'] = ex.
+            salida['error'] = ex
         return JsonResponse(salida, safe=False, status=status.HTTP_202_ACCEPTED)
 
 
