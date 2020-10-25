37e3dcdf158696aaae7f6b4f4acec8adaa6c04a0
sergio garcia <sergio.martinez-g@hotmail.com>
2020-10-25 04:55:32 +0100

    Update views.py

--- Api/inventary/views.py
+++ Api/inventary/views.py
@@ -127,7 +127,7 @@ class CreateListAddress(generics.ListCreateAPIView):
 
     def get(self, request, *args, **kwargs):
         id = request.GET['id']
-        address = getObject(Address.objects.filter(user__pk=id))
+        address = getObject(Address.objects.filter(user__pk=id).values())
         return JsonResponse(address, safe=False, status=status.HTTP_200_OK)
 
 
