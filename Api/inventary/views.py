7fd6edf51116985c6da3ee63be3d386eceec2692
sergio garcia <sergio.martinez-g@hotmail.com>
2020-10-27 17:43:55 +0100

    Update views.py

--- Api/inventary/views.py
+++ Api/inventary/views.py
@@ -172,9 +172,10 @@ class CreateListAddress(generics.ListCreateAPIView, generics.RetrieveDestroyAPIV
 
     def delete(self, request, *args, **kwargs):
         salida=dict()  
-        #_id =request.data['id']
-        _id =request.GET['id']
+        
         try:
+            #_id =request.data['id']
+            _id =request.GET['id']
             Address.objects.get(pk=_id).delete()
             salida['ok'] = True
         except Exception as ex:
